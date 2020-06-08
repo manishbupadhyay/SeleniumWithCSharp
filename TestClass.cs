eeda3490cebd1e89391d0aecdf712936e4aa3963
Manish Upadhyay <ermanishupadhyay@gmail.com>
2020-06-09 01:38:31 +0530

    Setup Selenium with C# Framework and run a simple test.

---
+++ TestClass.cs
@@ -0,0 +1,24 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumWithCSharp
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://www.facebook.com";
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("ermanishupadhyay@gmail.com");
+            driver.Quit();
+        }
+    }
+}
